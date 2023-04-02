197f0e031792279775cef927a99ec3c6fdc6481f
Arkadiusz Śpiewak <17320098+arekspiewak1991@users.noreply.github.com>
2023-04-02 16:50:02 +0200

    SPORT-1: Add initial webpack configuration to run React + TS based project (#1)

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.[contenthash].js",
+    path: path.resolve(__dirname, "build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "ts-loader",
+      },
+    ],
+  },
+  mode: "development",
+  resolve: {
+    extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+  devServer: {
+    port: 3000,
+    static: {
+      directory: path.join(__dirname, "build"),
+    }
+  },
+};
